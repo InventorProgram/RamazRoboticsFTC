e98adef787811a02a0dce7fc453eadd173dded18
InventorProgram <heilborn-nigrih@ramaz.org>
2024-11-08 06:47:06 -0500

    The teleop from the original repo is now uploaded. The location of Info.md has been changed as well

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+@TeleOp(name =  "FTC_bot_2024", group = "Iterative OpMode")
+
+public class TestTeleop extends OpMode {
+    Gamepad Gamepad = new Gamepad();
+    DcMotor frontLeftMotor = null;
+    DcMotor frontRightMotor =  null;
+    DcMotor backLeftMotor = null;
+    DcMotor backRightMotor = null;
+
+    @Override
+    public void init() { //This runs when hitting the init button on the driver station
+
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, " backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, " backRightMotor");
+    }
+
+    @Override
+    public void loop() { //This repeats when you hit the start button
+
+        double y = Gamepad.left_stick_y;
+        double x = -Gamepad.left_stick_x * 1.1;
+        double r = -Gamepad.right_stick_x;
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(r), 1);
+
+        double frontLeftPower = (y + x + r) / denominator;
+        double frontRightPower = (y - x - r) / denominator;
+        double backLeftPower = (y - x + r) / denominator;
+        double backRightPower = (y + x -r) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+    }
+}
