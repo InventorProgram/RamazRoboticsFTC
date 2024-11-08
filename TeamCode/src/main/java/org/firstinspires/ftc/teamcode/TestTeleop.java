53ddcfc2faf26dde7713ae06c94f6955f9ce957b
InventorProgram <heilborn-nigrih@ramaz.org>
2024-11-08 11:53:46 -0500

    arm and wrist motor functionality in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -14,7 +14,7 @@ public class TestTeleop extends OpMode {
     DcMotor backRightMotor;
     DcMotor armMotor;
     DcMotor wristMotor;
-    Double ticks; //Create ticks variable for each motor. Each motor has a number of ticks per rotation. This can be used to make half-turns
+    Double ticks = 100.0; //100.0 is a placeholder. Create ticks variable for each motor. Each motor has a number of ticks per rotation. This can be used to make half-turns
     Double newtarget;
 
     @Override
@@ -27,14 +27,22 @@ public class TestTeleop extends OpMode {
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backLeftMotor = hardwareMap.get(DcMotor.class, " backLeftMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, " backRightMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        wristMotor = hardwareMap.get(DcMotor.class,"wristMotor");
+
+        DcMotor[] motors = {frontLeftMotor,frontRightMotor,backLeftMotor,backRightMotor};
+        for (DcMotor motor: motors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 
     @Override
     public void loop() { //This repeats when you hit the start button
-        mecanum_set_power();
+        mecanum_input();
+        arm_input();
     }
 
-    public void mecanum_set_power() {
+    public void mecanum_input() { //Checks joystick input and accordingly sets power level to motors in the mecanum drivetrain
         double y = Gamepad1.left_stick_y;
         double x = -Gamepad1.left_stick_x * 1.1;
         double r = -Gamepad1.right_stick_x;
@@ -49,5 +57,16 @@ public class TestTeleop extends OpMode {
         frontRightMotor.setPower(frontRightPower);
         backLeftMotor.setPower(backLeftPower);
         backRightMotor.setPower(backRightPower);
+    }
+
+    public void arm_input(){
+        //up-down
+        if (Gamepad1.dpad_up) {
+            armMotor.setPower(1);
+        }
+
+        if (Gamepad1.dpad_down){
+            armMotor.setPower(-1);
+        }
     }
 }
