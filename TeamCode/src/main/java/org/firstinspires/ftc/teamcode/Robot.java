b5b353f65823e09f3737142f73ea07842114711d
InventorProgram <heilborn-nigrih@ramaz.org>
2024-11-25 18:48:09 -0500

    Small changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,7 +1,16 @@
+/*
+Documentation:
+insert-useful info here
+ */
+
 package org.firstinspires.ftc.teamcode;
 
 public class Robot {
+    int attribute;
+    public Robot(int parameter){
+        attribute = parameter;
+    }
     static void sampleMethod() {
-        System.out.println("Hello World");
+
     }
 }
