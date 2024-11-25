b933a6dae3c8c7985b4b8b0b4d066866d7a77856
InventorProgram <heilborn-nigrih@ramaz.org>
2024-11-25 14:32:02 -0500

    Autonomous, Robot classes created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousAutomaton.java
@@ -0,0 +1,16 @@
+/*
+Documentation:
+ */
+
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Into_the_Deep_Autonomous")
+
+public class AutonomousAutomaton extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Robot {
+    static void sampleMethod() {
+        System.out.println("Hello World");
+    }
+}
