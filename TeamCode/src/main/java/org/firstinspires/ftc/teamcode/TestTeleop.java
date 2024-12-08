5ebeb047e0fee36f33603e151cacc237b40d7d9c
InventorProgram <heilborn-nigrih@ramaz.org>
2024-12-08 00:01:16 -0500

    Version the night before the robotics competition! Super exciting!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -61,13 +61,6 @@ public class TestTeleop extends OpMode {
         double y = gamepad2.left_stick_y;
         double x = -gamepad2.left_stick_x * 1.1;
         double r = -gamepad2.right_stick_x;
-
-        /*
-        double y = -gamepad2.right_stick_x;
-        double x = gamepad2.right_stick_y * 1.1;
-        double r = gamepad2.left_stick_y;
-        */
-
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(r), 1);
 
         //Power variables calculated from joystick variables and denominator
